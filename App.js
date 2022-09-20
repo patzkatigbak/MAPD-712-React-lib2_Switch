7eec555c6b3577fe1cff6e580b68ab735d5eb46d
patzkatigbak <patzkatigbak@gmail.com>
2022-09-19 23:16:17 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Switch} from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+const [value, setValue] = React.useState('');
+const [isEnabled, setIsEnabled] = React.useState(false);
+const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View 
+        style={[styles.container,isEnabled ? {backgroundColor:'black' } : {backgroundColor:'white'} ]}>
+
+      <Text style={styles.textViewStatic}>Input Text below</Text>
+
+      <TextInput style={styles.edit}
+          clearTextFocus = 'true'
+          onChangeText = {text => setValue(text)}>
+      </TextInput>
+
+      <Text 
+          style={styles.textViewOutput}>{value}
+      </Text>
+
+      <Button 
+          title='Clear Output' 
+          onPress={() => setValue('')}>
+      </Button>
+
+      <Switch 
+          onValueChange={toggleSwitch}
+          trackColor={{false: 'red', true: 'gray'}}
+          thumbColor={isEnabled ? 'orange' : 'green'}
+          value={isEnabled}>
+      </Switch>
+      
     </View>
   );
 }
@@ -13,8 +40,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent: 'center'
   },
+  edit:{
+    borderWidth: 2,
+    borderColor:'orange',
+    width:'90%',
+    color:'blue'
+  },
+  textViewStatic:{
+    fontSize: 22,
+    color: "red"
+  },
+  textViewOutput:
+  {
+    color: 'blue'
+  }
 });
